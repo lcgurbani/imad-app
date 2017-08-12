dd8e881f19056d53f06d4b229b8abc286a6d0d44
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-12 22:31:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one', function (req, res)  {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res)  {
-   ndFile(path.join(__dirname, 'ui', 'article-two.html'));
+   ndFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/article-three', function (req, res)  {
    ndFile(path.join(__dirname, 'ui', 'article-three.html'));
