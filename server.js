10311ef9896e067d99d5ee158d1307ae81d4868c
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-16 23:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,9 @@ app.get('/', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+app.get('/ui/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname,'ui', 'style.css'));
+});
 app.get('/:articleName', function (req, res) {
 //articleName== article-one
 //articles[articleName] == contents of article one object
