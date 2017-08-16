cfccf281f6ba6596f897389ee019bbe20884ea72
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-17 00:05:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 //Change the tex of the main-text div
 
-var element = document.getelementbyId("main-text");
+var element = document.getElementById("main-text");
 Element.innerHTML ="New Value";
