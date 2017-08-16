908acf7a6bdeced4ecca408df8bdbf0291abab45
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-17 00:39:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById("main-text");
 element.innerHTML ="New Value";
 
 //move the image
-var element = document.getElementById("madi");
+var element = document.getElementById("img");
  img.onclick= function() {
      img .style.marginRight ="100px";
  };  
