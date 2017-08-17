40f553a35bdf9cb789b88a9af278a478b9e34a62
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-17 13:31:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,4 @@
-
-console.log('Loaded!');
-//Change the tex of the main-text div
-
-//var element = document.getElementById("main-text");
-//element.innerHTML ="New Value";
-
-//move the image
-var img = document.getElementById("madi");
-
-var marginLeft = 0;
-function moveRight () {
-    marginLeft= marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
- img.onclick= function() {
-     var interval =setInterval(moveRight,100);
- };  
- 
- //counter code
+//counter code
   var button =document.getElemenById("counter");
   var counter =0;
   button.onclick = function() {
@@ -26,7 +7,7 @@ function moveRight () {
       
       //Capture a  response  and store it in a variable
       
-      //render the variable in a correct span
+      //render the variable in the correct span
       counter=counter+1;
       var span =document.getElementById("count");
       span.innerHTML = counter.toString();
