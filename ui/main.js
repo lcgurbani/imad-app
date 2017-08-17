c13ef40cd25074f8a28839c48cc168c1719ff023
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-17 15:12:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function() {
     request.open('GET','http://lcgurbani12326.imad.hasura-app.io/counter' , true);
     request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var sumit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make a request and send the name to the server
+    
+    //Capture a list of names and render it as a list
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i< names.length;i++)
+    list +='<li>'+ names[i] + '</li>';
+};
+var ul=document.getElementById('namelist');
+ul.innerHTML = list;
+};
