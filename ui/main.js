857f467b006e21dccc83cd7594ce7d9eab55880f
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-17 14:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,21 +4,11 @@ var counter =0;
 button.onclick = function() {
     
     //Create the request
-        var request = new XMLHttpRequest();
+    
     //Capture the response and store the variable in it
-        request.onreadystatechange = function() {
-            if (request.readyState === XMLHttpRequest.DONE)  {
-                    //Take some action
-                    if (request.status ===200) {
-                       var counter = request.responseText();
-                       var span =document.getElementById('count');
-                        span.innerHTML = counter.toString();
-                    }
-            }
-            //not done yet
-        };
         
-     // Make the request
-     request.open('GET', 'http://lcgurbani12326.imad.hasura.io/counter', true);
-     request.send(null);
+    //Render the variable in the correct span
+     counter = counter +1;
+     var span = document.getElementById('count');
+     span.innerHTML = counter.toString();
 };
