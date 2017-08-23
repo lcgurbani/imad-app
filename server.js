f7ef6dea91c58ee2f0612e3e8bfb1d0728636fb7
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-23 21:24:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
 
+var config = {
+    user : 'lcgurbani12326',
+    database : lcgurbani12326,
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : processs.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
-var articles = { 
+var articles =  
 'article-one' : {
     title : 'Article one| lokesh Gurbani',
     heading : 'Article-one',
@@ -78,13 +86,24 @@ return  htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 counter =0;
 app.get('/counter', function (req, res) {
     counter = counter +1;
     res.send(counter.toString());
 });
 
+var  pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    //return a response with the results
+    pool.query ('SELECT * FROM test', function (err, result) {
+        if (err)
+            res.status(500).send(err.toString());
+        }    else{
+                res.send(JSON.stringify(result));
+        }
+    });
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
