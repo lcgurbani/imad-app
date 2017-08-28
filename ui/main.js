cb29c81c5771f070859b5968deb527103ac84268
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-28 22:32:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
 var button = document.getElementById('counter');
-var counter =0;
 button.onclick = function() {
-    //make a request to the counter endpoint
+    //Create  a request
+    var request = new XMLHttpRequest();
     
     //Capture the reesponse and store it in a variable
+    request.onreadystatechanged = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            //Take some action
+            if(status.request === 200) {
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+        }
+        ///Not done yet
+    };
     
-    //render tha vaariable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Make a request
+    request.open('GET', 'http://lcgurbani12326.imaad.hasura-app.io/counter', true);
+    request.send(null);
 };
