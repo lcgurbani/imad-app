67f9dd73a3ca39cc7f4b10cda7439212925c2743
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-08-28 23:39:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,33 +9,32 @@ button.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             //Take some action
             if(request.status === 200) {
-                var counter = request.responseText;
-                 var span = document.getElementById('count');
-                 span.innerHTML = counter.toString();
+               //submit the name
+            var nameInput = document.getElementById('name'); 
+            var name = nameInput.value;
+            var submit = document.getElementById('submit_btn');
+            submit.onclick = function() {
+            //Make a request to the server and send the name
+    
+            // capture a list of name and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i=0; i<names.length; i++) {
+            list += '<li>' + names[i] + '</li>';
+        }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+    
             }
         }
         ///Not done yet
     };
     
     // Make a request
-    request.open('GET', 'http://lcgurbani12326.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://lcgurbani12326.imad.hasura-app.io/submit-name?name=' +name, true);
     request.send(null);
 };
 
-//submit the name
-var nameInput = document.getElementById('name'); 
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    //Make a request to the server and send the name
-    
-    // capture a list of name and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-    list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    
+
 };
