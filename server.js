6889f6adc64d7d21d9f3c5bd87e0754dd22eebe9
lcgurbani <30714921+lcgurbani@users.noreply.github.com>
2017-09-06 22:18:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/ui/main.js', function (req, res) {
 
 function hash (input,salt) {
     //how do we create a hash?
-    var hshed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
     
 }
